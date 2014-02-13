6c04d3ccdf0b1fb0d9988c46e7c7801b1f090e14
DeLiS <delis91@gmail.com>
2014-02-13 18:04:01 +0200

    BinaryIO tests refactored

--- src/test/BinaryIOTests.java
+++ src/test/BinaryIOTests.java
@@ -7,10 +7,11 @@ import org.junit.Test;
 import algos.BinaryIO;
 public class BinaryIOTests {
 
-	byte[] data;
+    public static final int dataSize = 10000;
+    byte[] data;
 	@Before
 	public void setUp() throws Exception {
-		data = new byte[10000];
+		data = new byte[dataSize];
 	}
 
 	@Test
@@ -20,20 +21,37 @@ public class BinaryIOTests {
 		int size = 5001;
 		byte[] numbers = new byte[size];
 		int[] bits = new int[size];
-		for(int i=0;i<size;++i)
-		{
-			numbers[i] = (byte)(-127 + r.nextInt(250));
-			bits[i] = 12;
-		}
-		for(int i=0;i<size;++i)
-			bio.WriteBits(numbers[i] & 0xFF, bits[i]);
-		bio.Flush();
-		bio = new BinaryIO(data);
-		for(int i=0;i<size;++i)
-			assertEquals(numbers[i],(byte)bio.ReadBits(bits[i]));		
+
+        generateNumbers(r, size, numbers, bits);
+        writeNumbers(bio, size, numbers, bits);
+
+        readNumbersAndAssert(size, numbers, bits);
+
 	}
 
-	@Test
+    private void readNumbersAndAssert(int size, byte[] numbers, int[] bits) {
+        BinaryIO bio = new BinaryIO(data);
+        for(int i=0;i<size;++i){
+            assertEquals(numbers[i],(byte)bio.ReadBits(bits[i]));
+        }
+    }
+
+    private void writeNumbers(BinaryIO bio, int size, byte[] numbers, int[] bits) {
+        for(int i=0;i<size;++i){
+			bio.WriteBits(numbers[i] & 0xFF, bits[i]);
+        }
+        bio.Flush();
+    }
+
+    private void generateNumbers(Random r, int size, byte[] numbers, int[] bits) {
+        for(int i=0;i<size;++i)
+        {
+            numbers[i] = (byte)(-127 + r.nextInt(250));
+            bits[i] = 12;
+        }
+    }
+
+    @Test
 	public void testReadBits() {
 		
 	}
